3a8e6999bf603e64a56df03ed6224a7252d7cf24
BaoKha <baokhatranle@gmail.com>
2025-05-16 10:34:43 +0700

    [add] lab-01 ex01 ex02

---
+++ lab01/ex02/ex_02_06.py
@@ -0,0 +1,9 @@
+intput_str = input("Nhập X,Y : ")
+dimesions=[int(x) for x in intput_str.split(',')]
+rowNum=dimesions[0]
+colNum=dimesions[1]
+multilist = [[0 for col in range(colNum)] for row in range(rowNum)]
+for row in range(rowNum):
+    for col in range(colNum):
+        multilist[row][col]= row * col
+print (multilist)
